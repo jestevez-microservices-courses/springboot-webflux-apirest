8ac73023b6d910147ae5c2cdac1bf84c61131b9e
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-13 16:30:18 +0100

    New Spring Boot project - WebFlux ApiRest

---
+++ src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.msa.webflux.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
