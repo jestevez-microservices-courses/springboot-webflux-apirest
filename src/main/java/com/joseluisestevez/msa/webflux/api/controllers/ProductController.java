7a15ce8e6735fe4f032cbe604049f1c913bab44b
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-13 18:09:16 +0100

    Add create products endpoint

--- src/main/java/com/joseluisestevez/msa/webflux/api/controllers/ProductController.java
+++ src/main/java/com/joseluisestevez/msa/webflux/api/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package com.joseluisestevez.msa.webflux.api.controllers;
 
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,17 @@ public class ProductController {
         return productService.findById(id).map(p -> {
             return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(p);
         }).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<Product>> create(@RequestBody Product product) {
+
+        if (product.getCreateAt() == null) {
+            product.setCreateAt(new Date());
+        }
+
+        return productService.save(product)
+                .map(p -> ResponseEntity.created(URI.create("/api/products/".concat(p.getId()))).contentType(MediaType.APPLICATION_JSON).body(p));
+
     }
 }
