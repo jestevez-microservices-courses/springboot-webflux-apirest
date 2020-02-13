380ee18472b7d9a9221cec13d5fab6baf52a7ba6
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-13 17:57:41 +0100

    Implemented product rest api

---
+++ src/main/java/com/joseluisestevez/msa/webflux/api/controllers/ProductController.java
@@ -0,0 +1,35 @@
+package com.joseluisestevez.msa.webflux.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joseluisestevez.msa.webflux.api.models.documents.Product;
+import com.joseluisestevez.msa.webflux.api.service.ProductService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public Mono<ResponseEntity<Flux<Product>>> list() {
+        return Mono.just(ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(productService.findAll()));
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Product>> view(@PathVariable String id) {
+        return productService.findById(id).map(p -> {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(p);
+        }).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+}
