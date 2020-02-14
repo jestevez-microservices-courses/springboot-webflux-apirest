39b5f4d67eefa7f4f58c758f15fec8c5c117e36d
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-14 10:59:54 +0100

    Add upload products photo endpoint

--- src/main/java/com/joseluisestevez/msa/webflux/api/controllers/ProductController.java
+++ src/main/java/com/joseluisestevez/msa/webflux/api/controllers/ProductController.java
@@ -1,12 +1,16 @@
 package com.joseluisestevez.msa.webflux.api.controllers;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joseluisestevez.msa.webflux.api.models.documents.Product;
@@ -28,6 +33,19 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Value("${config.uploads.path}")
+    private String path;
+
+    @PostMapping("/uploads/{id}")
+    public Mono<ResponseEntity<Product>> uploads(@PathVariable String id, @RequestPart FilePart file) {
+        return productService.findById(id).flatMap(p -> {
+            p.setPhoto(UUID.randomUUID().toString() + "-" + file.filename().replace(" ", "").replace(":", "").replace("\\", ""));
+            return file.transferTo(new File(path + p.getPhoto())).then(productService.save(p));
+        }).map(p -> {
+            return ResponseEntity.ok(p);
+        }).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 
     @GetMapping
     public Mono<ResponseEntity<Flux<Product>>> list() {
