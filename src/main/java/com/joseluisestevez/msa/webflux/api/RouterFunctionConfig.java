f6330a099b2ec5a6872d12076d7f0500b1361cad
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-17 21:11:22 +0100

    Create functional endpoints with  RouterFunction and Handler

---
+++ src/main/java/com/joseluisestevez/msa/webflux/api/RouterFunctionConfig.java
@@ -0,0 +1,20 @@
+package com.joseluisestevez.msa.webflux.api;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.joseluisestevez.msa.webflux.api.handler.ProductHandler;
+
+@Configuration
+public class RouterFunctionConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> routes(ProductHandler productHandler) {
+        return route(GET("/api/v2/products").or(GET("/api/v3/products")), productHandler::list);
+    }
+}
---
+++ src/main/java/com/joseluisestevez/msa/webflux/api/handler/ProductHandler.java
@@ -0,0 +1,23 @@
+package com.joseluisestevez.msa.webflux.api.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.joseluisestevez.msa.webflux.api.models.documents.Product;
+import com.joseluisestevez.msa.webflux.api.service.ProductService;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class ProductHandler {
+
+    @Autowired
+    private ProductService productService;
+
+    public Mono<ServerResponse> list(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(productService.findAll(), Product.class);
+    }
+}
