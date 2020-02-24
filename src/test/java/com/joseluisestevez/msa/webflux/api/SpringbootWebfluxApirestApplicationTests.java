4503570ad11c2b3fb0be206d0e4f5796db162ace
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-24 14:17:51 +0100

    Test - list Endpoint

--- src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
+++ src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
@@ -1,13 +1,24 @@
 package com.joseluisestevez.msa.webflux.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.joseluisestevez.msa.webflux.api.models.documents.Product;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringbootWebfluxApirestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testList() {
+        webTestClient.get().uri("/api/products").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
+                .contentType(MediaType.APPLICATION_JSON).expectBodyList(Product.class).hasSize(9);
+    }
 
 }
