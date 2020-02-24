25cf8a8b1b2a511299a62a921eb719f2a1e48939
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-24 14:25:31 +0100

    Test - list Endpoint using consumeWith

--- src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
+++ src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
@@ -1,5 +1,8 @@
 package com.joseluisestevez.msa.webflux.api;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +21,15 @@ class SpringbootWebfluxApirestApplicationTests {
     @Test
     void testList() {
         webTestClient.get().uri("/api/products").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectHeader()
-                .contentType(MediaType.APPLICATION_JSON).expectBodyList(Product.class).hasSize(9);
+                .contentType(MediaType.APPLICATION_JSON).expectBodyList(Product.class).consumeWith(response -> {
+                    List<Product> products = response.getResponseBody();
+                    products.forEach(product -> {
+                        System.out.println(product.getName());
+                    });
+                    // Assertions.assertEquals(9, products.size());
+                    Assertions.assertEquals(false, products.isEmpty());
+                });
+        // .hasSize(9);
     }
 
 }
