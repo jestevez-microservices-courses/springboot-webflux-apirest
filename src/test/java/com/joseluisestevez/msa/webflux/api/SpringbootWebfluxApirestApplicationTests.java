82156ee8921a0b823d7c3832d608671088234f64
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-02-24 14:46:17 +0100

    Test - view product by name

--- src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
+++ src/test/java/com/joseluisestevez/msa/webflux/api/SpringbootWebfluxApirestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.joseluisestevez.msa.webflux.api;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,12 +12,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.joseluisestevez.msa.webflux.api.models.documents.Product;
+import com.joseluisestevez.msa.webflux.api.service.ProductService;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringbootWebfluxApirestApplicationTests {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @Autowired
+    private ProductService productService;
 
     @Test
     void testList() {
@@ -30,6 +35,15 @@ class SpringbootWebfluxApirestApplicationTests {
                     Assertions.assertEquals(false, products.isEmpty());
                 });
         // .hasSize(9);
+    }
+
+    @Test
+    void testView() {
+        String productName = "TV Panasonic Pantalla LCD";
+        Product product = productService.findByName(productName).block(); // block sincrono
+        webTestClient.get().uri("/api/products/{id}", Collections.singletonMap("id", product.getId())).accept(MediaType.APPLICATION_JSON).exchange()
+                .expectStatus().isOk().expectHeader().contentType(MediaType.APPLICATION_JSON).expectBody().jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.name").isEqualTo(productName);
     }
 
 }
